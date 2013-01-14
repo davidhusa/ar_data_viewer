9f3388ea521f3bf9aa50a71c6d764b1fe200b21b
David Husa <davidhusa@gmail.com>
2013-01-14 15:51:29 -0800

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArDataViewer::Application.config.secret_token = 'e0872f38cdbfe910eb46dbe84f4e60d2c31491e1027938860622e58fa048a2b242136d122f2453ff6182b9da99f6ff41e2a4aa7ac00b8bf1ffb71b7c057f2646'
